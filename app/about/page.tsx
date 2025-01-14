55ff8494b973cdbf54fc8dbe75175477db755c7f
Mayura Vivekananda <km.vivekananda@gmail.com>
2025-01-13 22:49:18 -0500

    Updated about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,10 +5,13 @@ export default function About() {
         <h1 className="text-2xl md:text-4xl font-bold text-center mb-4 pb-2 border-b-2 border-primary">
           About Us
         </h1>
-            
+            <h2 className="text-xl md:text-2xl text-center text-gray-300 mb-8">
+              A Market Edge Analytics Product
+            </h2>
+
             <div className="bg-black/30 backdrop-blur-sm rounded-xl p-8 mt-8 text-left">
               <p className="mb-4">
-                Trading Levels Algo was developed as a way to make my trading more consistent. I&apos;m a software engineer with experience trading across different markets.
+                Trading Levels Algo was developed by Market Edge Analytics as a way to make my trading more consistent. I&apos;m a software engineer with experience trading across different markets.
                 Through my trading journey, I realized that while I knew a lot about the how of trading, I wasn&apos;t as sharp on the execution.
                 That led me, at the beginning of this year, to start developing a bot to improve my trading performance. 
                 The bot came to life in May, and I&apos;ve been actively tracking and analyzing its performance since July.
